300e05d4a0fae72a4304b761b40c50b0424fb69e
Scott Balfour <scottbalf@gmail.com>
2022-09-13 16:39:00 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+
+const boom = document.getElementById("boom")
+
+document.addEventListener("keypress", (e) => {
+    if (e.key == "Numpad1"){
+        boom.play()
+    }
+})
+
+const clap = document.getElementById("clap")
+
+document.addEventListener("keypress", (e) => {
+    if (e.key == "Numpad2"){
+        clap.play
+    }
+})
+
+const hihat = document.getElementById("hihat")
+
+document.addEventListener("keypress", (e) => {
+    if (e.key == "Numpad3"){
+        hihat.play
+    }
+})
+
+const kick = document.getElementById("kick")
+
+document.addEventListener("keypress", (e) => {
+    if (e.key == "Numpad4"){
+        kick.play
+    }
+})
+
+const openhat = document.getElementById("openhat")
+
+document.addEventListener("keypress", (e) => {
+    if (e.key == "Numpad5"){
+        openhat.play
+    }
+})
+
+const ride = document.getElementById("ride")
+
+document.addEventListener("keypress", (e) => {
+    if (e.key == "Numpad6"){
+        ride.play
+    }
+})
+
+const snare = document.getElementById("snare")
+
+document.addEventListener("keypress", (e) => {
+    if (e.key == "Numpad7"){
+        snare.play
+    }
+})
+
+const tink = document.getElementById("tink")
+
+document.addEventListener("keypress", (e) => {
+    if (e.key == "Numpad8"){
+        tink.play
+    }
+})
+
+const tom = document.getElementById("tom")
+
+document.addEventListener("keypress", (e) => {
+    if (e.key == "Numpad9"){
+        tom.play
+    }
+})
